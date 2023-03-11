c460771d5e4ad5f799b1f47d33c800d580ef90a9
Ricardo Morales Gonzalez <rmorale5@jhu.edu>
2023-03-10 22:42:47 -0500

    added LoginHero to Ricardo page

--- src/pages/ricardo.tsx
+++ src/pages/ricardo.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import LoginHero from "~/components/loginHero";
 
 import { api } from "~/utils/api";
 
@@ -14,6 +15,7 @@ const Ricardo: NextPage = () => {
           <Link href="/" className="flex max-w-xs flex-col gap-4 text-blue-600 rounded-xl p-4 hover:bg-white/20"  >
               <p>Go To Home Page</p>
           </Link>
+          <LoginHero></LoginHero>
     </>
   );
 };
