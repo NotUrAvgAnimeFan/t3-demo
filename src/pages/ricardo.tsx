a8b97001239763f161e18bd555a77389bfb30151
Ricardo Morales Gonzalez <rmorale5@jhu.edu>
2023-03-10 19:57:39 -0500

    Update ricardo.tsx

--- src/pages/ricardo.tsx
+++ src/pages/ricardo.tsx
@@ -9,7 +9,7 @@ const Ricardo: NextPage = () => {
     <>
       
       <main className="">
-        <h1>This is Ricardo's page</h1>
+        <h1>This is Ricardo&apos;s page</h1>
           </main>
           <Link href="/" className="flex max-w-xs flex-col gap-4 text-blue-600 rounded-xl p-4 hover:bg-white/20"  >
               <p>Go To Home Page</p>
