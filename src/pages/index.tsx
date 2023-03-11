45352c317022b1d2623ce790c178e46cec16fade
Ricardo Morales Gonzalez <rmorale5@jhu.edu>
2023-03-10 22:28:53 -0500

    added some things

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,47 +56,48 @@ const Home: NextPage = () => {
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
           </p>
-
-          <div className="hero min-h-screen min-w-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-              <div className="text-center lg:text-left">
-                <h1 className="text-5xl font-bold">Login now!</h1>
-                <p className="py-6">
-                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                  assumenda excepturi exercitationem quasi. In deleniti eaque
-                  aut repudiandae et a id nisi.
-                </p>
-              </div>
-              <div className="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
-                <div className="card-body">
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">Email</span>
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="email"
-                      className="input-bordered input"
-                    />
-                  </div>
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">Password</span>
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="password"
-                      className="input-bordered input"
-                    />
-                    <label className="label">
-                      <a href="#" className="link-hover label-text-alt link">
-                        Forgot password?
-                      </a>
-                    </label>
-                  </div>
-                  <div className="form-control mt-6">
+        </div>
+        <div className="min-w-screen hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl font-bold">Login now!</h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+            </div>
+            <div className="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
+              <div className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="email"
+                    className="input-bordered input"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="password"
+                    className="input-bordered input"
+                  />
+                  <label className="label">
+                    <a href="#" className="link-hover label-text-alt link">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                <div className="form-control mt-6">
+                  <Link href="login">
                     <button className="btn-primary btn">Login</button>
-                  </div>
+                  </Link>
                 </div>
               </div>
             </div>
